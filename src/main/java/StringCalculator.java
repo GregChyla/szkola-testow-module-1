4a2917d337ecb383cecc22244ff0d9e9e3da6c7d
Grzegorz Chyla <gchyla@yameo.pl>
2020-07-18 11:58:15 +0200

    Initial commit

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    int add(String input) {
+        if (Objects.isNull(input) || input.isEmpty()) {
+            return 0;
+        } else {
+            String[] numbers = input.split(",");
+            int result = 0;
+
+            for (String number : numbers) {
+                if (Pattern.matches("^[-+]?\\d*$", number)) {
+                    result += Integer.parseInt(number);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @BeforeAll
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    void shouldReturn0WhenNullGiven() {
+        assertEquals(0, calculator.add(null));
+    }
+
+    @Test
+    void shouldReturn0WhenZeroGiven() {
+        assertEquals(0, calculator.add("0"));
+    }
+
+    @Test
+    void shouldReturn0WhenLetterGiven() {
+        assertEquals(0, calculator.add("A"));
+    }
+
+    @Test
+    void shouldReturnNegativeNumberWhenNegativeNuberGiven() {
+        assertEquals(-1, calculator.add("-1"));
+    }
+
+    @Test
+    void shouldReturnCorrectSum() {
+        assertEquals(3, calculator.add("2,1"));
+        assertEquals(3, calculator.add("3"));
+    }
+
+    @Test
+    void shouldReturnCorrectSumWithNegativeNumbersGive() {
+        assertEquals(-1, calculator.add("-2,1"));
+        assertEquals(-33, calculator.add("-33"));
+    }
+}
